ca32b9c26f32c907b598c71556d53835b8b0cd4a
kaveeshanethruwan <kaveesha.james151@gmail.com>
2023-10-09 19:47:41 +0530

    Added enum

---
+++ enum.ts
@@ -0,0 +1,18 @@
+// You can use enums to represent a set of predefined values in your code
+// enum is read only
+
+enum ResourceType {
+  book,
+  author,
+  film,
+  director,
+  person,
+}
+
+// By default enum assign with a number start with 0.
+// But you can assign values too.
+
+enum CustomResourceType {
+  book = 10,
+  author = "mac",
+}
