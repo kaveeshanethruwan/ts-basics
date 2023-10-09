3f5ff39d87211238360c9a31ae758907ddba49f1
kaveeshanethruwan <kaveesha.james151@gmail.com>
2023-10-09 20:19:19 +0530

    Added tuples

---
+++ tuples.ts
@@ -0,0 +1,14 @@
+//  tuples are a data structure that allows you to store ordered collections of elements
+// of different types. Unlike arrays, where elements can be of the same type, tuples enable you to specify a fixed number of elements, each with a distinct
+// type
+
+let myTuple: [string, number, boolean];
+myTuple = ["welcome", 42, true];
+console.log(myTuple[0]); // Output: 'hello'
+console.log(myTuple[1]); // Output: 42
+console.log(myTuple[2]); // Output: true
+
+// Note
+// 1. fixed length
+// 2. type safety
+// 3. array like operator - can access array methods
